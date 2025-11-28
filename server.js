729c5bbd3b086345594631cd67934f67861390de
seojun01 <parkseojoon@naver.com>
2025-11-28 15:31:05 +0900

    Routing

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.listen(8080, () => {
+  console.log("http://localhost:8080 에서 서버 실행중");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/about.html");
+});
